2401103d71cd04ba203dbed72eb4b4ec63067104
HAMDOUCHI <mohamed.hamdouchi@capgemini.com>
2025-05-15 17:14:51 +0200

    Add workspace tag and make variables required

--- resources.tf
+++ resources.tf
@@ -19,7 +19,7 @@ locals {
   common_tags = {
     Environment = var.environment
     billingCode = var.billing_code
-    Workspace = terraform.Workspace
+    Workspace   = terraform.workspace
   }
 }
 
